7d0918c5ffa6eba1b271c894ca3665f187f0ba5e
Cristhian <jobs.cluna@gmail.com>
2022-10-07 00:30:53 -0500

    style: fix border styles

--- src/pages/Home/sections/About/About.jsx
+++ src/pages/Home/sections/About/About.jsx
@@ -7,17 +7,17 @@ const About = () => {
    <section className='about-section'>
       <div className='left-ab'>
         <h1>About Me!</h1>
-        <p> Jared Christensen is well-seasoned (mostly salty) designer, an annoyingly enthusiastic music fan, a gold-medalist couch potato, a hopeful landscape gardener, and a work in progress.</p>
+        <p className='capital'> Jared Christensen is well-seasoned (mostly salty) designer, an annoyingly enthusiastic music fan, a gold-medalist couch potato, a hopeful landscape gardener, and a work in progress.</p>
 
         <h4>Profesionally</h4>
-        <p>I'm passionate about designing software that is human-friendly, clear, purposeful, easy to use, and aesthetically pleasing. I've worked with a variety of clients including TaxAct, Pepsico, The Container Store, FedEx, and 7-Eleven. From workflow diagrams to detailed user interface designs, I love shaping and crafting on-screen experiences.</p>
+        <p>I'm passionate about designing software that is human-friendly, clear, purposeful, easy to use, and aesthetically pleasing. I've worked with a variety of clients in.</p>
 
         <h4>Personally</h4>
-        <p>I love music, and often write about my loves and discoveries here on my blog. I know: how old-fashioned is that? All of my music consumption is meticulously catalogued on Last.fm, and I maintain a Spotify playlist of the music I’m really into right now, because I’m sure you’re dying to know.</p>
+        <p>I love music, and often write about my loves and discoveries here on my blog. I know: how old-fashioned is that? All of my music consumption is meticulously catalogued on Last.fm, and I maintain a Spotify playli</p>
       </div>
       <div className='right-ab'>
         <div className='info-ab'>
-            <span className='logo-title'>Social Media</span>
+            <p className='logo-title'>Social Media</p>
             <div className='logo-ab'>
                 <Twitter  className='logo-item'/>
                 <Github  className='logo-item'/>
@@ -29,7 +29,7 @@ const About = () => {
             <div className='info-ab'>
                 <img className='info-img' src="https://lastfm.freetls.fastly.net/i/u/174s/61a6649f35502d1674c91a74205326b5.jpg"/>
                 <span className='info-title'>GAMING</span>
-                <span>Exit to darkness</span>
+                <span >Exit to darkness</span>
             </div>
 
             <div className='info-ab'>
