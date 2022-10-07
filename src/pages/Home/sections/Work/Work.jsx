b292a1357eb529f09801abf0257d296be89f0fde
Cristhian <jobs.cluna@gmail.com>
2022-10-06 23:42:40 -0500

    feat: add work comp section

--- src/pages/Home/sections/Work/Work.jsx
+++ src/pages/Home/sections/Work/Work.jsx
@@ -5,13 +5,13 @@ const Work = () => {
   return (
     <section className='work-section'>
       <div className='cv'>
-        Donwload CV
+        <button>Download CV</button>
       </div>
 
       <div className='exp-wrapper'>
         <div className='exp-item left'>
             <div className='col'>
-                <h1>Experience</h1>
+                <h3 className='col-h1'>Experience</h3>
                 <div className='col-info'>
                     <span className='col-title'>Title</span>
                     <span className='col-date'>Date Time</span>
@@ -37,14 +37,40 @@ const Work = () => {
 
         <div className='exp-item right'>
             <div className='col'>
-                Row -right
+                <h3 className='col-h1'>Education</h3>
+                <div className='col-info'>
+                    <span className='col-title'>Title</span>
+                    <span className='col-date'>Date Time</span>
+                    <p className='col-p'>The User Interfaces were designed through Figma and Sketch, in consensus with the end users, and the product owner</p>
+                    <p className='col-p'>The User Interfaces were designed through Figma and Sketch, in consensus with the end users, and the product owner</p>
+                </div>
+
+            </div>
+
+            <div className='col'>
+                <h3 className='col-h1'>Education</h3>
+                <div className='col-info'>
+                    <span className='col-title'>Title</span>
+                    <span className='col-date'>Date Time</span>
+                    <p className='col-p'>The User Interfaces were designed through Figma and Sketch, in consensus with the end users, and the product owner</p>
+                    <p className='col-p'>The User Interfaces were designed through Figma and Sketch, in consensus with the end users, and the product owner</p>
+                </div>
+
+                <div className='col-info'>
+                    <span className='col-title'>Title</span>
+                    <span className='col-date'>Date Time</span>
+                    <p className='col-p'>The User Interfaces were designed through Figma and Sketch, in consensus with the end users, and the product owner</p>
+                    <p className='col-p'>The User Interfaces were designed through Figma and Sketch, in consensus with the end users, and the product owner</p>
+                </div>
+
+
             </div>
         </div>
-      </div>
+    </div>
 
-      <div className='cv'>
-        Donwload CV
-      </div>
+    <div className='cv'>
+        <button>Download CV</button>
+    </div>
 
     </section>
   )
