500e6bd4e55df0bad10904b67bce8868a39b1bcc
Cristhian <jobs.cluna@gmail.com>
2022-10-08 15:17:40 -0500

    feat: add social media links

--- src/pages/Home/sections/About/About.jsx
+++ src/pages/Home/sections/About/About.jsx
@@ -1,41 +1,44 @@
 import React from 'react'
 import './About.css'
 import {FiGithub as Github, FiLinkedin as Linkedin, FiCodepen as Codepen, FiTwitter as Twitter} from 'react-icons/fi'
+import Data from '../../../../utils/Dummy.js'
 
 const About = () => {
+
+  const {socialMedia} = Data
   return (
    <section className='about-section'>
       <div className='left-ab'>
         <h1>About Me!</h1>
-        <p className='capital'> Jared Christensen is well-seasoned (mostly salty) designer, an annoyingly enthusiastic music fan, a gold-medalist couch potato, a hopeful landscape gardener, and a work in progress.</p>
+        <p className='capital'> Cristhian Luna is well-seasoned developer, an annoyingly enthusiastic music fan, a gold-medalist couch potato, a hopeful landscape gardener, and a work in progress.</p>
 
         <h4>Profesionally</h4>
-        <p>I'm passionate about designing software that is human-friendly, clear, purposeful, easy to use, and aesthetically pleasing. I've worked with a variety of clients in.</p>
+        <p>Web developer with 2+ years of experience building web apps for different local and US SMEs in multiple sectors. With an emphasis on details, translating layouts into an efficient, reusable, readable & secure code.</p>
 
         <h4>Personally</h4>
-        <p>I love music, and often write about my loves and discoveries here on my blog. I know: how old-fashioned is that? All of my music consumption is meticulously catalogued on Last.fm, and I maintain a Spotify playli</p>
+        <p>I love music, and often draw y spend time reading fantasy medieval books like Lord of the rings, Ice & Fire song, The Witcher. How old-fashioned is that? All of my music consumption is meticulously catalogued on a Spotify playlist</p>
       </div>
       <div className='right-ab'>
         <div className='info-ab'>
             <p className='logo-title'>Social Media</p>
             <div className='logo-ab'>
-                <Twitter  className='logo-item'/>
-                <Github  className='logo-item'/>
-                <Codepen className='logo-item'/>
-                <Linkedin  className='logo-item'/>
+                <Twitter onClick={()=>window.open(socialMedia.twitter, "_blank")}  className='logo-item'/>
+                <Github onClick={()=>window.open(socialMedia.twitter, "_blank")}  className='logo-item'/>
+                <Codepen onClick={()=>window.open(socialMedia.twitter, "_blank")} className='logo-item'/>
+                <Linkedin onClick={()=>window.open(socialMedia.twitter, "_blank")}  className='logo-item'/>
             </div>
         </div>
         <div className='info-holder'>
             <div className='info-ab'>
                 <img alt='logo-spotify' className='info-img' src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png"/>
                 <span  className='info-title'>My fav Tracks</span>
-                <a href='https://open.spotify.com/playlist/37i9dQZF1EUMDoJuT8yJsl?si=ea6d84e259664159&nd=1' target="_blank">Spotify tracks</a>
+                <a href={socialMedia.spotify} target="_blank">Spotify tracks</a>
             </div>
 
             <div className='info-ab'>
-                <img alt='logo-theWitcher3' className='info-img' src="https://image.api.playstation.com/vulcan/img/rnd/202009/2913/TQKAd8U6hnIFQIIcz6qnFh8C.png"/>
+                <img alt='logo-theWitcher3' className='info-img' src={socialMedia.game.img}/>
                 <span className='info-title'>My fav Game</span>
-                <a href='https://store.steampowered.com/app/292030/The_Witcher_3_Wild_Hunt/' target="__blank">The Witcher 3</a>
+                <a href={socialMedia.game.link} target="__blank">{socialMedia.game.name}</a>
             </div>
         </div>
             
