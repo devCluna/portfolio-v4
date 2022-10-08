b292081c0aa49948614e6b7b7edd2a5d1b906870
Cristhian <jobs.cluna@gmail.com>
2022-10-08 16:57:43 -0500

    feat: add footer elements

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './Footer.css'
+import {FiGithub as Github, FiLinkedin as Linkedin, FiCodepen as Codepen, FiTwitter as Twitter} from 'react-icons/fi'
+import Data from '../../utils/Dummy'
 
 const Footer = () => {
+    const {socialMedia} = Data
   return (
     <footer>
-        Footer
+        <span>Designed & Developed by <a onClick={()=> window.open(socialMedia.linkedin)}>@devCluna</a></span>
+        <span>Copyright © <a onClick={()=> window.open(socialMedia.linkedin)}>devCluna</a></span>
+        <div className='social-holder'>
+            <span className='social-media'>Social Media</span>
+            <div className='icons-holder'>
+                <Github className='foot-icon'  onClick={()=> window.open(socialMedia.github)}/>
+                <Twitter className='foot-icon'  onClick={()=> window.open(socialMedia.twitter)}/>
+                <Linkedin className='foot-icon'  onClick={()=> window.open(socialMedia.linkedin)}/>
+                <Codepen className='foot-icon'  onClick={()=> window.open(socialMedia.codepen)}/>
+            </div>
+        </div>
   </footer>
   )
 }
