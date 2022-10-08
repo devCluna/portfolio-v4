4af54e1a136c082bf963a749651a454061e0a707
Cristhian <jobs.cluna@gmail.com>
2022-10-08 16:58:03 -0500

    feat: add mailto funct

--- src/pages/Home/sections/Contact/Contact.jsx
+++ src/pages/Home/sections/Contact/Contact.jsx
@@ -7,9 +7,10 @@ const Contact = () => {
     <section className='section contact-section'>
         <span className='contact-comment'>What's next?</span>
         <h1 className='contact-title'>Get in touch!</h1>
-        <p className='contact-p'>I'm currently looking for new opportunities, so Im open mind to new projects. I'll try my best to reply, as soon as possible</p>
-
-        <Button context={"Say hi!"}/>
+        <p className='contact-p'>I'm currently looking for new opportunities, so I'm open mind to new projects. I'll try my best to reply, as soon as possible</p>
+        <a href='mailto:jobs.cluna@gmail.com'>
+          <Button context={"Say hi!"} />
+        </a>
     </section>
   )
 }
